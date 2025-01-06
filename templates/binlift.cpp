0d8e2bf0b724cfdf3e0a885067f16e122ad00a78
JeffLegendPower <80235181+JeffLegendPower@users.noreply.github.com>
2025-01-06 17:58:28 -0500

    Update binlift.cpp

--- templates/binlift.cpp
+++ templates/binlift.cpp
@@ -1,32 +1,27 @@
 int jmp[20][MAXN], dep[MAXN];
 
 // add child to parent
-void addchild(int parent, int child){
-	jmp[0][child] = parent;
-	dep[child] = dep[parent]+1;
+void addchild(int p, int v) {
+	jmp[0][v] = p;
+	dep[v] = dep[p] + 1;
 }
 
 // prepare jumps
-void build(){
-	for(int i=0; i<20; ++i)
-		for(int j=0; j<MAXN; ++j)
-			jmp[i][j] = jmp[i-1][jmp[i-1][j]];
+void build() {
+	for (int i = 0; i < 20; i++) for(int j = 0; j < MAXN; j++) jmp[i][j] = jmp[i-1][jmp[i-1][j]];
 }
 
 // kth ancestor of i
-int kth(int i, int k){
-	for(int x=19; x>=0; --x)
-		if(k & (1<<x)) i = jmp[x][i];
+int kth(int i, int k) {
+	for(int x = 19; x >= 0; x--) if (k & (1<<x)) i = jmp[x][i];
 	return i;
 }
 
 // LCA of a, b
-int lca(int a, int b){
-	if(dep[a] < dep[b]) std::swap(a, b);
+int lca(int a, int b) {
+	if(dep[a] < dep[b]) swap(a, b);
 	a = kth(a, dep[a] - dep[b]);
 	if(a == b) return a;
-	for(int x=19; x>=0; --x)
-		if(jmp[x][a] != jmp[x][b])
-			a = jmp[x][a], b = jmp[x][b];
+	for(int x = 19; x >= 0; x--) if(jmp[x][a] != jmp[x][b]) a = jmp[x][a], b = jmp[x][b];
 	return jmp[0][a];
 }
