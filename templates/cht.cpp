dbf9727c0b4cd0fa7e1feaafd2084ce68b599c75
JeffLegendPower <80235181+JeffLegendPower@users.noreply.github.com>
2025-01-06 18:03:33 -0500

    Create cht.cpp

---
+++ templates/cht.cpp
@@ -0,0 +1,14 @@
+// Can replace with any other function as long as any f[i] intersects with any other f[j] at most once
+struct line {
+    ll a, b;
+
+    ll calc(ll x) { return a*x + b; }
+    ld intersect(line two) { return (ld) (two.b - b) / (a - two.a); };
+};
+
+deque<line> hull;
+vector<int> ints(MAXN+1);
+// iota(ints.begin(), ints.end(), 0);
+
+auto cmp = [&hull](int idx, ll x) { return hull[idx].intersect(hull[idx + 1]) < x; };
+// int idx = *lower_bound(ints.begin(), ints.begin() + hull.size() - 1, x, cmp);
