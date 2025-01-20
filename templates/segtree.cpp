544d56319ccf6312ae416c197d2fcfb58695d1aa
JeffLegendPower <80235181+JeffLegendPower@users.noreply.github.com>
2025-01-20 17:05:26 -0500

    Update segtree.cpp

--- templates/segtree.cpp
+++ templates/segtree.cpp
@@ -1,30 +1,31 @@
+template <class T>
 class Segtree {
 private:
     int N;  // array size
-    vector<ll> t;
+    vector<T> t;
 
-    ll oper(ll a, ll b) {
+    T combine(T a, T b) {
         return a + b;
     }
 
 public:
-    Segtree(int size) : N(size), t(2 * size, 0ll) {}
+    Segtree(int size) : N(size), t(2 * size, T()) {}
 
     void build() {  // Build the tree
         for (int i = N - 1; i > 0; i--) 
-            t[i] = oper(t[i<<1], t[i<<1|1]);
+            t[i] = combine(t[i<<1], t[i<<1|1]);
     }
 
-    void modify(int p, ll value) {  // Set value at position p
+    void modify(int p, T value) {  // Set value at position p
         for (t[p += N] = value; p > 1; p >>= 1) 
-            t[p >> 1] = oper(t[p], t[p^1]);
+            t[p >> 1] = combine(t[p], t[p^1]);
     }
 
-    ll query(int l, int r) {  // Query on interval [l, r)
-        ll res = 0;
+    T query(int l, int r) {  // Query on interval [l, r)
+        T res = T();
         for (l += N, r += N; l < r; l >>= 1, r >>= 1) {
-            if (l&1) res = oper(res, t[l++]);
-            if (r&1) res = oper(res, t[--r]);
+            if (l&1) res = combine(res, t[l++]);
+            if (r&1) res = combine(res, t[--r]);
         }
         return res;
     }
